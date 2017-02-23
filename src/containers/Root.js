c5b76ce486d50496bf82693378d1beb2c38ec315
Sophie Gill <sgill@andigital.com>
2017-02-23 12:18:16 +0000

    first commit

---
+++ src/containers/Root.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class Root extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1 className='welcome-header'>Welcome to React!</h1>
+      </div>
+    )
+  }
+}
+
+export default Root;
